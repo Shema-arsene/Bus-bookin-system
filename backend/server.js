6ab4c0dd424b9c101d234adeedf030591ac8c16b
Shema Arsene <aniyonkurushema@gmail.com>
2025-07-14 19:33:51 +0200

    Add buses routes in the server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
-// const busRoutes = require("./routes/busRoutes.js")
 const busRoutes = require("./routes/busRoutes")
 const connectDB = require("./config/db")
 
@@ -12,12 +11,12 @@ dotenv.config()
 const app = express()
 app.use(express.json()) // Parse incoming JSON
 
-// app.get("/", (req, res) => {
-//   res.send("Bus Booking API Running")
-// })
+app.get("/", (req, res) => {
+  res.send("Bus Booking API Running")
+})
 
 // Routes
-// app.use("/api/buses", busRoutes)
+app.use("/api/buses", busRoutes)
 
 // connectDB()
 
