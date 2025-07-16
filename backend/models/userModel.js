a20ec388d2515509edfaa521379bfd4c128b9c1f
Shema Arsene <aniyonkurushema@gmail.com>
2025-07-16 20:10:04 +0200

    Update userModel

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
 
 const userSchema = new mongoose.Schema(
   {
-    name: {
+    fullName: {
       type: String,
       required: true,
     },
@@ -10,28 +11,29 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
       unique: true,
-      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
     },
     phone: {
       type: String,
       required: true,
     },
-    isAdmin: {
-      type: Boolean,
-      default: false,
-    },
   },
   { timestamps: true }
 )
 
+// Hash password before saving
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next()
   this.password = await bcrypt.hash(this.password, 10)
   next()
 })
 
-userSchema.methods.comparePassword = async function (candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password)
+// Compare passwords
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password)
 }
 
 module.exports = mongoose.model("User", userSchema)
