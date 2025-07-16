18baa6d78c5a385f7d5965d70ee5d0ac1e52be6c
Shema Arsene <aniyonkurushema@gmail.com>
2025-07-16 11:17:38 +0200

    Add hashing password and password compare functionality

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -24,4 +24,14 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 )
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next()
+  this.password = await bcrypt.hash(this.password, 10)
+  next()
+})
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password)
+}
+
 module.exports = mongoose.model("User", userSchema)
