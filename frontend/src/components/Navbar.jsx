74e44d6d8160309959e9acc30f7ae0cdd5cfd961
Shema Arsene <aniyonkurushema@gmail.com>
2025-07-20 10:33:05 +0200

    Update user Navbar component to accomodate loggedin user

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,32 @@
-import React, { useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { Link, NavLink } from "react-router-dom"
 import { Menu, X } from "lucide-react"
+import { useAuth } from "../context/AuthContext"
 
 const Navbar = () => {
+  const { isAuthenticated, user } = useAuth()
+
   const [isMenuOpen, setIsMenuOpen] = useState(false)
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen)
   }
+  const [isUserMenuOpen, setIsUserMenuOpen] = useState(false)
 
+  const userMenuRef = useRef(null)
+
+  // Open and close userMenu
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (userMenuRef.current && !userMenuRef.current.contains(event.target)) {
+        setIsUserMenuOpen(false)
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside)
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [])
   return (
     <header className="flex items-center justify-between py-4 px-10 bg-white border-b border-gray-300 text-black">
       <img src="./public/logo.jpg" alt="Logo" className="h-12" />
@@ -43,12 +62,73 @@ const Navbar = () => {
               )}
             </NavLink>
           ))}
-          <Link
+          {isAuthenticated ? (
+            <>
+              <div
+                className="relative flex items-center justify-center cursor-pointer mx-10"
+                onClick={() => setIsUserMenuOpen((prev) => !prev)}
+                ref={userMenuRef}
+              >
+                <User />
+                <button
+                  onClick={() => setIsUserMenuOpen((prev) => !prev)}
+                  className="flex items-center space-x-1 text-gray-600 hover:text-red-600 transition-colors"
+                >
+                  <span
+                    className="text-sm font-medium uppercase"
+                    onClick={() => setIsUserMenuOpen((prev) => !prev)}
+                  >
+                    {user?.fullName}
+                  </span>
+                </button>
+
+                {isUserMenuOpen && (
+                  <div className="absolute top-10 -right-4 mt-2 w-48 bg-white rounded-md shadow-xl z-10 overflow-hidden">
+                    <NavLink
+                      to="/profile"
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-red-100"
+                      onClick={() => setIsUserMenuOpen(false)}
+                    >
+                      Profile
+                    </NavLink>
+                    {user?.role === "admin" && (
+                      <NavLink
+                        to="/admin"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-red-100"
+                        onClick={() => setIsUserMenuOpen(false)}
+                      >
+                        Admin Dashboard
+                      </NavLink>
+                    )}
+                    <button
+                      onClick={() => {
+                        logout()
+                        setIsUserMenuOpen(false)
+                      }}
+                      className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-red-100"
+                    >
+                      Logout
+                    </button>
+                  </div>
+                )}
+              </div>
+            </>
+          ) : (
+            <>
+              <Link
+                to="/login"
+                className="text-white bg-sky-400 hover:bg-sky-800 font-semibold px-4 py-2 rounded-md transition-colors duration-300"
+              >
+                Login
+              </Link>
+            </>
+          )}
+          {/* <Link
             to="/login"
             className="text-white bg-sky-400 hover:bg-sky-800 font-semibold px-4 py-2 rounded-md transition-colors duration-300"
           >
             Login
-          </Link>
+          </Link> */}
         </div>
 
         {/* Toggle Menu Button */}
