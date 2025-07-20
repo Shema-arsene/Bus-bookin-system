244784c0d6cf445843509a85792da8c89ac1554e
Shema Arsene <aniyonkurushema@gmail.com>
2025-07-20 19:43:02 +0200

    Add fetching journey by userID route

--- backend/routes/journeyRoutes.js
+++ backend/routes/journeyRoutes.js
@@ -1,9 +1,9 @@
-// routes/journeyRoutes.js
 const express = require("express")
 const router = express.Router()
 const {
   createJourney,
   getAllJourneys,
+  getJourneysByUser,
   getJourneyById,
   updateJourney,
   deleteJourney,
@@ -13,6 +13,8 @@ const protect = require("../middleware/authMiddleware")
 // Public Routes
 router.get("/", getAllJourneys)
 router.get("/:id", getJourneyById)
+
+router.get("/user/:userId", getJourneysByUser)
 
 // Admin/Protected Routes (optional: add role check later)
 router.post("/add", protect, createJourney)
