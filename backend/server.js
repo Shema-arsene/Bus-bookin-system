41417e88320634f6630416f330fcc780455cc071
Shema Arsene <aniyonkurushema@gmail.com>
2025-07-20 17:14:24 +0200

    Add cors to the server

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const journeyRoutes = require("./routes/journeyRoutes")
 const busRoutes = require("./routes/busRoutes")
 const bookingRoutes = require("./routes/bookingRoutes")
 const userRoutes = require("./routes/userRoutes")
+const cors = require("cors")
 
 dotenv.config()
 
@@ -13,6 +14,16 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(express.json()) // Parse incoming JSON
+
+app.use(
+  cors({
+    // origin: process.env.CLIENT_URL || "*",
+    origin: "http://localhost:5176", // or your actual frontend URL
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+)
 
 app.get("/", (req, res) => {
   res.send("Bus Booking API Running")
