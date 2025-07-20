d6178d5bb629586fb5430bf89fadaac9878fb762
Shema Arsene <aniyonkurushema@gmail.com>
2025-07-20 16:57:37 +0200

    Add database URL to all API paths

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -3,25 +3,27 @@ export const BASE_URL = import.meta.env.VITE_BASE_URL
 // utils/apiPaths.js
 export const API_PATHS = {
   AUTH: {
-    LOGIN: "api/auth/login",
-    REGISTER: "api/auth/register",
-    GET_USER_INFO: "api/auth/getUser",
+    LOGIN: `${BASE_URL}/api/auth/login`,
+    REGISTER: `${BASE_URL}/api/auth/register`,
+    GET_USER_INFO: `${BASE_URL}/api/auth/getUser`,
   },
   JOURNEYS: {
-    GET_ALL_JOURNEYS: "api/journeys/",
-    GET_ALL_JOURNEYBYID: (journeyId) => `api/journeys/${journeyId}`,
-    ADD_JOURNEY: "api/journeys/add",
-    UPDATE_JOURNEY: (journeyId) => `api/journeys/update/${journeyId}`,
-    DELETE_JOURNEY: (journeyId) => `api/journeys/delete/${journeyId}`,
+    GET_ALL_JOURNEYS: `${BASE_URL}/api/journeys/`,
+    GET_ALL_JOURNEYBYID: (journeyId) => `${BASE_URL}/api/journeys/${journeyId}`,
+    ADD_JOURNEY: `${BASE_URL}/api/journeys/add`,
+    UPDATE_JOURNEY: (journeyId) =>
+      `${BASE_URL}/api/journeys/update/${journeyId}`,
+    DELETE_JOURNEY: (journeyId) =>
+      `${BASE_URL}/api/journeys/delete/${journeyId}`,
   },
   BOOKING: {
-    BOOK_SEATS: "api/booking",
+    BOOK_SEATS: `${BASE_URL}/api/booking`,
   },
   BUSES: {
-    GET_ALL_BUSES: "api/buses/",
-    GET_BUS_BY_ID: (busId) => `api/buses/${busId}`,
-    ADD_BUS: "api/buses",
-    UPDATE_BUS: (busId) => `api/buses/${busId}`,
-    DELETE_BUS: (busId) => `api/buses/${busId}`,
+    GET_ALL_BUSES: `${BASE_URL}/api/buses/`,
+    GET_BUS_BY_ID: (busId) => `${BASE_URL}/api/buses/${busId}`,
+    ADD_BUS: `${BASE_URL}/api/buses`,
+    UPDATE_BUS: (busId) => `${BASE_URL}/api/buses/${busId}`,
+    DELETE_BUS: (busId) => `${BASE_URL}/api/buses/${busId}`,
   },
 }
